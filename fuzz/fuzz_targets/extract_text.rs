d54ee27ef1d668c2ddd5982c109ad34dda7d4ea6
4812571 <micah.harrington9@gmail.com>
2023-05-02 22:23:58 -0400

    added file fuzzer

---
+++ fuzz/fuzz_targets/extract_text.rs
@@ -0,0 +1,18 @@
+#![no_main]
+
+use std::fs;
+use std::path::Path;
+use libfuzzer_sys::fuzz_target;
+use pdf_extract::extract_text;
+
+fuzz_target!(|data: &[u8]| {
+    let path = Path::new("test.pdf");
+    let pdf = String::from_utf8_lossy(data);
+
+    match fs::write(path, pdf) {
+        Ok(_) => (),
+        Err(_) => return,
+    }
+
+    let _ = extract_text(path);
+});
