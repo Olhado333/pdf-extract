21500f19531b4ae792f0b967dd31e1d41eecc185
4812571 <micah.harrington9@gmail.com>
2023-05-02 22:34:58 -0400

    fixed fuzz

--- fuzz/fuzz_targets/extract_text.rs
+++ fuzz/fuzz_targets/extract_text.rs
@@ -7,7 +7,7 @@ use pdf_extract::extract_text;
 
 fuzz_target!(|data: &[u8]| {
     let path = Path::new("test.pdf");
-    let pdf = String::from_utf8_lossy(data);
+    let pdf = String::from_utf8_lossy(data).to_string();
 
     match fs::write(path, pdf) {
         Ok(_) => (),
